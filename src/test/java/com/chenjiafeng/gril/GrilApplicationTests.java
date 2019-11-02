961210f2791c3315dc699d207e52b5e5bff6ab58
tyler.chen <M_chenjiafeng@163.com>
2019-11-02 18:30:13 +0800

    提交创建的SpringBoot-gril

---
+++ src/test/java/com/chenjiafeng/gril/GrilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenjiafeng.gril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
