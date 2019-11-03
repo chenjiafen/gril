6f0e332c5f1b0095883242f128205941d24b8455
tyler.chen <M_chenjiafeng@163.com>
2019-11-03 16:13:14 +0800

    @RequestParam("id") Integer myId 传统的?id=10的用发

--- src/main/java/com/chenjiafeng/gril/HelloController.java
+++ src/main/java/com/chenjiafeng/gril/HelloController.java
@@ -2,10 +2,7 @@ package com.chenjiafeng.gril;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -21,11 +18,11 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "hello/{id}",method = RequestMethod.GET)
-    public String say(@PathVariable("id") Integer id){
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String say(@RequestParam("id") Integer myId){
 //        return "Hello Spring boot";
 //        return "这个女生使用的Size是"+size+"，年龄是"+age;
 //        return content;
-        return "id为："+id+",这个女生使用的Size是"+girlProperties.getCupSzie()+",年龄是"+girlProperties.getAge();
+        return "id为："+myId+",这个女生使用的Size是"+girlProperties.getCupSzie()+",年龄是"+girlProperties.getAge();
     }
 }
