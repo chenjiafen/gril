85a14eda9efbb4aacbf5f18d5b2a712ccdbf4874
tyler.chen <M_chenjiafeng@163.com>
2019-11-03 15:43:19 +0800

    value = {"/hello","/hi"}，访问不同路径返回的结果相同

--- src/main/java/com/chenjiafeng/gril/HelloController.java
+++ src/main/java/com/chenjiafeng/gril/HelloController.java
@@ -20,7 +20,7 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
     public String say(){
 //        return "Hello Spring boot";
 //        return "这个女生使用的Size是"+size+"，年龄是"+age;
